c7466ac5f0b1c0dbeb3f1fcd858b86799f1b7d88
Chih-Wei Huang <cwhuang@linux.org.tw>
2014-02-14 16:54:36 +0800

    Retry the hook library until successful

--- src/libhoudini_hook.cpp
+++ src/libhoudini_hook.cpp
@@ -20,11 +20,18 @@ extern "C" void dvmPlatformInvoke(void *pEnv, void *clazz, int argInfo, int argc
 
 namespace houdini {
 
+static void *(*h_dvmHookDlopen)(const char *, int, bool *) = 0;
+static int  (*h_dvmHookJniOnload)(bool, void *, void *, void *) = 0;
+static bool (*h_dvmHookCheckMethod)(void *) = 0;
+static void (*h_dvmHookCreateActivity)(bool, void *, void *, void *, void *, size_t) = 0;
+static void (*h_dvmHookPlatformInvoke)(void *, void *, int, int, const int *, const char *, void *, void *) = 0;
+static void *(*h_dvmHookDlsym)(bool, void *, const char *) = 0;
+
 static void *get_symbol(const char *symbol)
 {
-    static void *h_handle = dlopen("libdvm_houdini.so", RTLD_LAZY);
+    static void *h_handle = 0;
 
-    if (!h_handle) {
+    if (!h_handle && !(h_handle = dlopen("libdvm_houdini.so", RTLD_LAZY))) {
         ALOGE("Unable to open libdvm_houdini lib");
         return 0;
     }
@@ -36,35 +43,41 @@ static void *get_symbol(const char *symbol)
     return func;
 }
 
+static void hookInit()
+{
+    h_dvmHookDlopen = (void *(*)(const char *, int, bool *)) get_symbol("_ZN7houdini10hookDlopenEPKciPb");
+    h_dvmHookJniOnload = (int (*)(bool, void *, void *, void *)) get_symbol("_ZN7houdini13hookJniOnloadEbPvS0_S0_");
+    h_dvmHookCheckMethod = (bool (*)(void *)) get_symbol("_ZN7houdini15hookCheckMethodEPv");
+    h_dvmHookCreateActivity = (void (*)(bool, void *, void *, void *, void *, size_t)) get_symbol("_ZN7houdini18hookCreateActivityEbPvS0_S0_S0_j");
+    h_dvmHookPlatformInvoke = (void (*)(void *, void *, int, int, const int *, const char *, void *, void *)) get_symbol("_ZN7houdini21dvmHookPlatformInvokeEPvS0_iiPKiPKcS0_S0_");
+    h_dvmHookDlsym = (void *(*)(bool, void *, const char *)) get_symbol("_ZN7houdini9hookDlsymEbPvPKc");
+}
+
 void *hookDlopen(const char *filename, int flag, bool *useHoudini)
 {
-    static void *(*h_dvmHookDlopen)(const char *, int, bool *) =
-            (void *(*)(const char *, int, bool *)) get_symbol("_ZN7houdini10hookDlopenEPKciPb");
+    if (!h_dvmHookDlopen) {
+        hookInit();
+    }
 
     return h_dvmHookDlopen ? h_dvmHookDlopen(filename, flag, useHoudini) : dlopen(filename, flag);
 }
 
 int hookJniOnload(bool useHoudini, void *func, void *jniVm, void *arg)
 {
-    static int (*h_dvmHookJniOnload)(bool, void *, void *, void *) =
-            (int (*)(bool, void *, void *, void *)) get_symbol("_ZN7houdini13hookJniOnloadEbPvS0_S0_");
-
     return h_dvmHookJniOnload ? h_dvmHookJniOnload(useHoudini, func, jniVm, arg) : ((int (*)(void *, void *))func)(jniVm, arg);
 }
 
 bool hookCheckMethod(void *fnPtr)
 {
-    static bool (*h_dvmHookCheckMethod)(void *) =
-            (bool (*)(void *)) get_symbol("_ZN7houdini15hookCheckMethodEPv");
-
     return h_dvmHookCheckMethod ? h_dvmHookCheckMethod(fnPtr) : false;
 }
 
 void hookCreateActivity(bool useHoudini, void *createActivityFunc,
         void *activity, void *houdiniActivity, void *savedState, size_t savedStateSize)
 {
-    static void (*h_dvmHookCreateActivity)(bool, void *, void *, void *, void *, size_t) =
-            (void (*)(bool, void *, void *, void *, void *, size_t)) get_symbol("_ZN7houdini18hookCreateActivityEbPvS0_S0_S0_j");
+    if (!h_dvmHookCreateActivity) {
+        hookInit();
+    }
     if (h_dvmHookCreateActivity) {
         h_dvmHookCreateActivity(useHoudini, createActivityFunc, activity, houdiniActivity, savedState, savedStateSize);
     } else {
@@ -75,9 +88,6 @@ void hookCreateActivity(bool useHoudini, void *createActivityFunc,
 void dvmHookPlatformInvoke(void *pEnv, void *clazz, int argInfo, int argc,
         const int *argv, const char *shorty, void *func, void *pReturn)
 {
-    static void (*h_dvmHookPlatformInvoke)(void *, void *, int, int, const int *, const char *, void *, void *) =
-            (void (*)(void *, void *, int, int, const int *, const char *, void *, void *)) get_symbol("_ZN7houdini21dvmHookPlatformInvokeEPvS0_iiPKiPKcS0_S0_");
-
     if (h_dvmHookPlatformInvoke) {
 	h_dvmHookPlatformInvoke(pEnv, clazz, argInfo, argc, argv, shorty, func, pReturn);
     } else {
@@ -87,9 +97,6 @@ void dvmHookPlatformInvoke(void *pEnv, void *clazz, int argInfo, int argc,
 
 void *hookDlsym(bool useHoudini, void *handle, const char *symbol)
 {
-    static void *(*h_dvmHookDlsym)(bool, void *, const char *) =
-            (void *(*)(bool, void *, const char *)) get_symbol("_ZN7houdini9hookDlsymEbPvPKc");
-
     return h_dvmHookDlsym ? h_dvmHookDlsym(useHoudini, handle, symbol) : dlsym(handle, symbol);
 }
 
